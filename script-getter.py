0f02f0771971135672f0e2bab6e04674839b5218
Shaunak G <gadkari.shaunak@gmail.com>
2022-02-06 20:07:54 +1100

    Added comments

--- script-getter.py
+++ script-getter.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Fandom wiki pages with the Harry Potter transcripts on them.
 script_links = [
     "https://warnerbros.fandom.com/wiki/Harry_Potter_and_the_Philosopher%27s_Stone/Transcript",
     "https://warnerbros.fandom.com/wiki/Harry_Potter_and_the_Chamber_of_Secrets/Transcript",
@@ -14,11 +15,16 @@ script_links = [
 
 script = ""
 
+# Fetch each one
 for i in script_links:
     print("Getting script from: " + i)
 
+    # Parse the HTML content with BeautifulSoup
     page = BeautifulSoup(requests.get(i).content, 'html.parser')
+
+    # Get the element that holds the actual transcript and add the text content to the variable
     script += page.find('div', {'class': 'mw-parser-output'}).getText() + "\n"
 
+# Write the script to a file
 with open('hp-script.txt', 'w') as f:
     f.write(script)
